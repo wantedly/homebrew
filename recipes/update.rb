44bae89c84b112b2cf61cff1f42aa2d9a3ab1c8c
Seigo Uchida <spesnova@gmail.com>
2014-02-26 10:46:39 +0900

    Separate update from default recipe.

--- recipes/update.rb
+++ recipes/update.rb
@@ -4,7 +4,7 @@
 # Cookbook Name:: homebrew
 # Recipes:: default
 #
-# Copyright 2011-2013, Opscode, Inc.
+# Copyright 2011-2014, Opscode, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,20 +18,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
-extend(Homebrew::Mixin)
-
-homebrew_go = "#{Chef::Config[:file_cache_path]}/homebrew_go"
-owner = homebrew_owner
-
-Chef::Log.debug("Homebrew owner is '#{homebrew_owner}'")
-
-remote_file homebrew_go do
-  source 'https://raw.github.com/Homebrew/homebrew/go/install'
-  mode 00755
+package 'git' do
+  not_if 'which git'
 end
 
-execute homebrew_go do
+execute 'update homebrew from github' do
   user owner
-  not_if { ::File.exist? '/usr/local/bin/brew' }
+  command '/usr/local/bin/brew update || true'
 end
